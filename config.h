fb1f4f60d77178247e95fee3ee2f3674a6499bf3
kuring <361118339@qq.com>
2021-08-17 00:52:42 +0800

    add font LXGW WenKai Mono

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static       int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
+static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16", "LXGW WenKai Mono:size=16" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";

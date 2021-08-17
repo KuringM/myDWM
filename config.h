eb8d02e028f2f8a79b791d4e5b7993a33ab7d382
kuring <361118339@qq.com>
2021-08-17 23:16:57 +0800

    brave

--- config.h
+++ config.h
@@ -117,7 +117,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
 /* scripts control */

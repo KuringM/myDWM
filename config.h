df309b9d9c6f5501a932ba58d5510e501c7d6ce4
kuring <361118339@qq.com>
2021-08-19 21:15:35 +0800

    change dmenu launch

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char *fonts[]          = {
 	"LXGW WenKai Mono:size=16",
 	"JoyPixels:pixelsize=16:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
+//static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -119,7 +119,7 @@ static const Layout layouts[] = {
 /* commands */
 /* start application */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-g", "3", "-l", "15", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
@@ -202,6 +202,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = suspendcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = touchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = slcokcmd } },
+	{ MODKEY,                       XK_grave,  spawn,          SHCMD("dmenuunicode") },
 
 
 	/* change layouts */
